1d7621b6848930a84165ccf5ae3b0f0121e618eb
Cihan Sungur <sungur1988@gmail.com>
2022-09-17 00:40:25 +0300

    RepositoryInterfacesCreated

---
+++ Core/Repositories/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class, BaseEntity, new()
+    {
+
+        Task<TEntity> GetById(int id);
+        Task<IEnumerable<TEntity>> GetAll(Expression<Func<TEntity, bool>> predicate = null);
+
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task AddAsync(TEntity entity);
+
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+
+        void RemoveRange(IEnumerable<TEntity> entities);
+
+        TEntity Update(TEntity entity);
+    }
+}
