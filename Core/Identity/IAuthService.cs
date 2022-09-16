ca96761bcfc72a411c8a4538c7d3cfb7085479ab
Cihan Sungur <sungur1988@gmail.com>
2022-09-17 01:05:13 +0300

    AuthServiceAbstractAdded

---
+++ Core/Identity/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Identity
+{
+    public interface IAuthService
+    {
+        Task<RegisterResponseDto> Register(RegisterRequestDto registerRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
---
+++ Core/Identity/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Dtos;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Identity
+{
+    public interface IUserService
+    {
+        Task<AppUser> CreateUserAsync(RegisterRequestDto registerRequestDto);
+        Task<AppUser> GetUser(string email);
+        Task<bool> Login(AppUser user, string password);
+        Task<IList<Claim>> GetClaims(AppUser appUser);
+        Task<IList<string>> GetRoles(AppUser appUser);
+    }
+}
